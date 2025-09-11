77568044a4d80d0bd8322a8bfcff241e8eac03fb
aaudicio <aaudicio@frba.utn.edu.ar>
2025-09-10 23:41:18 -0300

    Delete - borrado logico de hechos

--- src/main/java/ar/edu/utn/dds/k3003/controller/HechoController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/HechoController.java
@@ -28,13 +28,28 @@ public class HechoController {
 
     @GetMapping("/{id}")
     public ResponseEntity<HechoDTO> obtener(@PathVariable String id) {
-        HechoDTO dto = fachada.buscarHechoXId(id);
+        Hecho h = hechos.findById(id).orElseThrow();
+
+        if ("borrado".equalsIgnoreCase(h.getEstado())) {
+            return ResponseEntity.notFound().build(); // no lo mostramos si está borrado
+        }
+
+        HechoDTO dto = new HechoDTO(
+                h.getId(),
+                h.getColeccionId(),
+                h.getTitulo(),
+                h.getEtiquetas(),
+                h.getCategoria(),
+                h.getUbicacion(),
+                h.getFecha(),
+                h.getOrigen()
+        );
+
         return ResponseEntity.ok(dto);
     }
 
     @PostMapping("/{id}/pdi")
     public ResponseEntity<PdIDTO> agregarPDI(@PathVariable String id, @RequestBody PdIDTO body) {
-        //body.hechoId=id;
         PdIDTO creado = fachada.agregar(body);
         return ResponseEntity.ok(creado);
     }
@@ -42,7 +57,7 @@ public class HechoController {
     @PatchMapping("/{id}")
     public ResponseEntity<HechoDTO> patch(@PathVariable String id, @RequestBody PatchHechoEstadoRequest body) {
         Hecho h = hechos.findById(id).orElseThrow();
-        if (body.estado() != null && !body.estado().isBlank()) { //por si es "borrado"
+        if (body.estado() != null && !body.estado().isBlank()) {
             h.setEstado(body.estado());
         }
         Hecho saved = hechos.save(h);
@@ -58,5 +73,25 @@ public class HechoController {
         );
         return ResponseEntity.ok(dto);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable String id) {
+        Hecho h = hechos.findById(id).orElseThrow();
+        h.setEstado("borrado"); // borrado lógico
+        hechos.save(h);
+
+        return ResponseEntity.noContent().build(); // no devolvemos el registro
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> eliminarTodos() {
+        var lista = hechos.findAll();
+
+        lista.forEach(h -> h.setEstado("borrado")); // marcamos todos como borrados
+        hechos.saveAll(lista);
+
+        return ResponseEntity.noContent().build(); // no devolvemos nada
+    }
+
 
 }
