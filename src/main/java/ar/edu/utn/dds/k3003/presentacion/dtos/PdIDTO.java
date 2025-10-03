1e9aaf01d3c99516b0e8d307a7e3045c26fcd106
dario <dgatica@frba.utn.edu.ar>
2025-10-03 19:50:48 -0300

    Add url to PdIDTO

--- src/main/java/ar/edu/utn/dds/k3003/presentacion/dtos/PdIDTO.java
+++ src/main/java/ar/edu/utn/dds/k3003/presentacion/dtos/PdIDTO.java
@@ -1,5 +1,6 @@
 package ar.edu.utn.dds.k3003.presentacion.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,29 +9,24 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PdIDTO {
 
-    public PdIDTO(String id, String hechoId, String descripcion, String lugar,
-          LocalDateTime momento, String contenido, List<String> etiquetas) {
-        this.id = id;
-        this.hechoId = hechoId;
-        this.descripcion = descripcion;
-        this.lugar = lugar;
-        this.momento = momento;
-        this.contenido = contenido;
-        this.etiquetas = etiquetas;
+    public PdIDTO(String id, String hechoId) {
+        this(id, hechoId, null, null, null, null, null, List.of());
     }
 
-    public PdIDTO(String id, String hechoId) {
-        this(id, hechoId, null, null, null, null, List.of());
+    public PdIDTO(String id, String hechoId, String url) {
+        this(id, hechoId, url, null, null, null, null, List.of());
     }
 
     public PdIDTO(String id) {
-        this(id, null, null, null, null, null, List.of());
+        this(id, null, null,null, null, null, null, List.of());
     }
 
     private String id;
     private String hechoId;
+    private String url;
     private String descripcion;
     private String lugar;
     private LocalDateTime momento;
